e2bbe70533cfaa96ec9b853e64e62507221b3fba
황광회 <ghl92479@gmail.com>
2025-06-12 13:37:29 +0900

    Update google_serializer.py

--- api_server/api/versioned/v1/users/social/serializers/google_serializer.py
+++ api_server/api/versioned/v1/users/social/serializers/google_serializer.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class GoogleVerificationSerializer(serializers.Serializer):
-    access_token = serializers.CharField(label='Google Access Token', help_text='구글 연동 로그인시 발급 받은 `Access-token`')
+    id_token = serializers.CharField(label='Google Access ID Token', help_text='구글 연동 로그인시 발급 받은 `ID-token`')
