e047fffb19b095b450b3eb5ac441333d0f10c4c9
황광회 <ghl92479@gmail.com>
2025-06-30 16:09:29 +0900

    Update models.py

--- api_server/users/models.py
+++ api_server/users/models.py
@@ -4,7 +4,6 @@ from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from django.db.models import UniqueConstraint, Q
 from django.utils.translation import gettext_lazy as _
-from model_utils import Choices
 from model_utils.managers import SoftDeletableManager
 from model_utils.models import UUIDModel, SoftDeletableModel, TimeStampedModel
 
