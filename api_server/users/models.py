be931335ccbfaacc140df6bb7937d46c55213c7d
황광회 <ghl92479@gmail.com>
2025-06-30 11:07:41 +0900

    Update: Model 추가 및 수정

--- api_server/users/models.py
+++ api_server/users/models.py
@@ -7,19 +7,8 @@ from model_utils import Choices
 from model_utils.managers import SoftDeletableManager
 from model_utils.models import UUIDModel, SoftDeletableModel
 
+from api_server.users.choices import MBTI_TYPE, IMAGE_SCALE, GENDER_TYPE
 
-GENDER_TYPE = Choices(
-    ('M', 'MALE', '남자'),
-    ('F', 'FEMALE', '여자'),
-    ('U', 'OTHER', '미제공'),
-)
-
-IMAGE_SCALE = Choices(
-    ('RAW', 'RAW', '원본'),  # 원본
-    ('DETAIL', 'DETAIL', '상세'),  # 상세보기
-    ('NORMAL', 'NORMAL', '일반'),  # 일반
-    ('THUMBNAIL', 'THUMBNAIL', '미리보기'),  # 미리보기
-)
 
 class UserManager(SoftDeletableManager, BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -35,12 +24,16 @@ class UserManager(SoftDeletableManager, BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None):
-        user = self.create_user(username, password=password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(UUIDModel, AbstractUser, SoftDeletableModel):
     user_id = models.CharField(
@@ -96,28 +89,19 @@ class User(UUIDModel, AbstractUser, SoftDeletableModel):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='사용자')
     nickname = models.CharField(max_length=60, null=True, blank=True, verbose_name='사용자 닉네임')
-    link = models.URLField(max_length=255, null=True, blank=True, verbose_name='개인 사이트 링크')
+    profile_image_link = models.URLField(max_length=255, null=True, blank=True, verbose_name='프로필 이미지 링크')
     gender = models.CharField(max_length=1, verbose_name='성별', null=True, blank=True, choices=GENDER_TYPE)
-    birthday = models.DateField(verbose_name='생일', null=True, blank=True)
+    birthday = models.DateTimeField(verbose_name='생일', null=True, blank=True, help_text='사주 컨텐츠를 활용하기 위해 시간까지 활용')
+    mbti = models.CharField(max_length=4, null=True, blank=True, verbose_name='MBTI', choices=MBTI_TYPE)
     introduce = models.TextField(verbose_name='프로필 소개', null=True, blank=True)
 
-    @property
-    def image_sets(self):
-        images_by_scale = {}
-        for img in self.images.all():
-            if img.scale in images_by_scale:
-                continue
-            images_by_scale[img.scale] = img
 
-        # 이미지를 가져오면서 대체 이미지 처리
-        raw = images_by_scale.get(IMAGE_SCALE.RAW)
-        detail = images_by_scale.get(IMAGE_SCALE.DETAIL) or raw
-        normal = images_by_scale.get(IMAGE_SCALE.NORMAL) or detail
-        thumbnail = images_by_scale.get(IMAGE_SCALE.THUMBNAIL) or normal
 
-        return {
-            'RAW': raw,
-            'DETAIL': detail,
-            'NORMAL': normal,
-            'THUMBNAIL': thumbnail,
-        }
+class UserImage(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='images', verbose_name='프로필')
+    image_url = models.URLField(max_length=255, verbose_name='이미지 URL')
+    scale = models.CharField(max_length=10, choices=IMAGE_SCALE, verbose_name='이미지 스케일')
+
+    class Meta:
+        verbose_name = '사용자 이미지'
+        verbose_name_plural = verbose_name
