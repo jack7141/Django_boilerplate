0bcb97f4d12d5bb9fe57a4ef32590d68f9ad765d
황광회 <jack7141@nate.com>
2025-07-17 17:26:42 +0900

    Update viewset.py

--- api_server/common/viewset.py
+++ api_server/common/viewset.py
@@ -1,4 +1,5 @@
 from django.http.request import QueryDict
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 
 
@@ -48,3 +49,44 @@ class MappingViewSetMixin(object):
         if self.filterset_class_map.get(self.action, None):
             return self.filterset_class_map[self.action]
         return getattr(self, 'filterset_class', None)
+
+
+class MappingDjangoFilterBackend(DjangoFilterBackend):
+    def get_filterset_class(self, view, queryset=None):
+        """
+        Return the `FilterSet` class used to filter the queryset.
+        """
+        filterset_class_map = getattr(view, "filterset_class_map", {})
+        filterset_class = filterset_class_map.get(view.action, None)
+
+        if filterset_class:
+            return filterset_class
+
+        filterset_class = getattr(view, "filterset_class", None)
+        filterset_fields = getattr(view, "filterset_fields", None)
+
+        if filterset_class:
+            filterset_model = filterset_class._meta.model
+
+            # FilterSets do not need to specify a Meta class
+            if filterset_model and queryset is not None:
+                assert issubclass(
+                    queryset.model, filterset_model
+                ), "FilterSet model %s does not match queryset model %s" % (
+                    filterset_model,
+                    queryset.model,
+                )
+
+            return filterset_class
+
+        if filterset_fields and queryset is not None:
+            MetaBase = getattr(self.filterset_base, "Meta", object)
+
+            class AutoFilterSet(self.filterset_base):
+                class Meta(MetaBase):
+                    model = queryset.model
+                    fields = filterset_fields
+
+            return AutoFilterSet
+
+        return None
